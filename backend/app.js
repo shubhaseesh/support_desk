5cd430cd2f78d2edf98b4a95ea4b4bbb77264406
shubhaseesh <shubhaseesh09@gmail.com>
2023-07-18 23:57:27 +0530

    setup db connection

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
-
+const connectDB = require('./config/db')
 const app = express();
 
 const PORT = process.env.PORT;
-
+connectDB()
 app.get("/", (req, res) => {
   res.json({ message: "5678" });
 });
